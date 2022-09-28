4e0cbda8b6c5f953b911c68614990270e5752f81
David Bootle <davidtbootle@gmail.com>
2022-09-28 18:10:49 -0400

    Added 404 page

---
+++ src/Error404.vue
@@ -0,0 +1,15 @@
+<script>
+
+</script>
+
+<template>
+<section id="404-container">
+    <h1>404</h1>
+    <p>Sorry, but that page doesn't exist.</p>
+    <a href="/"><button>Go Home</button></a>
+</section>
+</template>
+
+<style>
+
+</style>
--- src/router.js
+++ src/router.js
@@ -1,11 +1,13 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from './Home.vue'
 import Poll from './Poll.vue'
+import Error404 from './Error404.vue'
 
 export const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: "/", component: Home },
-    { path: "/poll/:id", component: Poll }
-]
+    { path: "/poll/:id", component: Poll },
+    { path: '/:pageParams(.*)*', component: Error404 },
+  ]
 });
