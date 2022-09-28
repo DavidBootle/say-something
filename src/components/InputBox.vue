d4122cedaa08eb7ae5bd79ebfab69d1a6fe50778
David Bootle <davidtbootle@gmail.com>
2022-09-27 22:10:38 -0400

    Added styling to InputBox

--- src/components/InputBox.vue
+++ src/components/InputBox.vue
@@ -15,14 +15,43 @@
 
 .input-box {
     background-color: #EEE;
-    padding: 2em;
+    padding: 2rem;
+    border-radius: 1.5rem;
 
     display: flex;
     align-items: center;
     justify-content: center;
     flex-direction: column;
     
-    width: 30%;
+    width: 18rem;
     margin: 0 auto;
+
+    font-family: sans-serif;
 }
+
+.input-box h1 {
+    margin: 0 0 0.5rem 0;
+    padding: 0;
+}
+
+.input-box h2 {
+    margin: 1.2rem 0 0.5rem 0;
+}
+
+.input-box input {
+    border-radius: 0.5rem;
+    outline: none;
+    border: none;
+    box-shadow: #CCC 2px 2px;
+    height: 2rem;
+    font-size: 20px;
+    text-align: center;
+
+    transition: box-shadow 0.2s linear;
+}
+
+.input-box input:hover, .input-box input:focus {
+    box-shadow: #AAA 4px 4px;
+}
+
 </style>
