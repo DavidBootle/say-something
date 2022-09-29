70ef2301f390e6ef8d14c6a3f4549dd401d3c1d4
David Bootle <davidtbootle@gmail.com>
2022-09-28 23:13:33 -0400

    Updated poll with actual graphics

--- src/Poll.vue
+++ src/Poll.vue
@@ -7,7 +7,7 @@ export default {
             pollId: this.$route.params.id,
             poll: null,
             loaded: false,
-            error: false
+            error: false,
         }
     },
     methods: {
@@ -52,10 +52,54 @@ export default {
 
 <template>
 
-<h1>{{ poll }}</h1>
+<section id="page-container">
+    <div v-if="loaded">
+        <div class="topic-box-container">
+            <h1 class="page-title">Say Something <strong>{{ poll.adjective }}</strong> About <strong>{{ poll.topic }}</strong></h1>
+            <textarea id="opinion-input"></textarea>
+        </div>
+    </div>
+</section>
 
 </template>
 
-<style>
+<style scoped>
+
+.page-title {
+    text-align: center;
+    font-family: sans-serif;
+    font-size: 3rem;
+    margin: 0;
+}
+
+.topic-box-container {
+    margin: 2rem auto;
+    background-color: #EEE;
+    padding: 2rem;
+    border-radius: 1rem;
+    max-width: 60rem;
+}
+
+.topic-box-container textarea {
+    margin: 1.5rem 0 0 0;
+    border-radius: 0.5rem;
+    min-height: 6rem;
+    resize: none;
+    font-size: 1rem;
+    width: 100%;
+    font-family: sans-serif;
+    padding: 1rem;
+    box-sizing: border-box;
+    text-align: center;
+}
+
+@media screen and (max-width: 1050px) {
+    .topic-box-container {
+        margin: 0.5rem;
+    }
+    .topic-box-container textarea {
+        padding: 0.5rem 0.2rem;
+    }
+}
 
 </style>
