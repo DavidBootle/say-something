fc946db483f0967d8fbd2f105ff820b84d5a700e
David Bootle <davidtbootle@gmail.com>
2022-10-03 16:39:44 -0400

    Added qrcode component

---
+++ src/components/QRCode.vue
@@ -0,0 +1,30 @@
+<script>
+
+import QrCreator from 'qr-creator'
+
+export default {
+    props: {
+        text: String,
+        size: Number
+    },
+    mounted() {
+        QrCreator.render({
+            text: this.text,
+            radius: 0,
+            ecLevel: 'M',
+            fill: '#000000',
+            background: null,
+            size: this.size,
+        }, this.$refs.qrBox)
+    }
+}
+
+</script>
+
+<template>
+    <div ref="qrBox" id="qr-box"></div>
+</template>
+
+<style>
+    
+</style>
