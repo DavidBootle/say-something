f6f65948639aa06c4e6a6828eb6ec09b7227c27c
David Bootle <davidtbootle@gmail.com>
2022-10-05 11:14:43 -0400

    Added emtpy input handling with visual feedback

--- src/components/SaySomethingBox.vue
+++ src/components/SaySomethingBox.vue
@@ -17,13 +17,24 @@ export default {
             buttonDisabled: false,
             backendURL: import.meta.env.VITE_BACKEND_URL,
             showQRCode: false,
-            currentURL: window.location.href
+            currentURL: window.location.href,
+            invalidInput: false,
         }
     },
     methods: {
         async createOpinion() {
+
+            // check for empty input
+            if (/^[ \n\r\t]*$/.test(this.opinionText)) {
+                this.invalidInput = true
+                return
+            } else {
+                this.invalidInput = false
+            }
+
             // disable submit button
             this.buttonDisabled = true;
+
             try {
                 // send request to create new opinion to the database
                 let response = await fetch(this.backendURL + '/new-opinion', {
@@ -67,7 +78,8 @@ export default {
 <template>
     <div class="topic-box-container">
         <h1 class="page-title">Say Something <strong>{{ adjective }}</strong> About <strong>{{ topic }}</strong></h1>
-        <textarea id="opinion-input" v-model="opinionText"></textarea>
+        <textarea id="opinion-input" v-model="opinionText" :class="{ invalid: invalidInput }"></textarea>
+        <p v-if="invalidInput" id="invalid-input-text">Your message cannot be empty</p>
         <button id="say-it-button" @click="createOpinion" :disabled="buttonDisabled">Say It!</button>
         <span id="openQRModal" @click="toggleQRCode" title="Toggle QR Code" :class="{ active: showQRCode }">
             <svg height="24" width="24"><path d="M13 21v-2h2v2Zm-2-2v-5h2v5Zm8-3v-4h2v4Zm-2-4v-2h2v2ZM5 14v-2h2v2Zm-2-2v-2h2v2Zm9-7V3h2v2ZM4.5 7.5h3v-3h-3ZM3 9V3h6v6Zm1.5 10.5h3v-3h-3ZM3 21v-6h6v6ZM16.5 7.5h3v-3h-3ZM15 9V3h6v6Zm2 12v-3h-2v-2h4v3h2v2Zm-4-7v-2h4v2Zm-4 0v-2H7v-2h6v2h-2v2Zm1-5V5h2v2h2v2ZM5.25 6.75v-1.5h1.5v1.5Zm0 12v-1.5h1.5v1.5Zm12-12v-1.5h1.5v1.5Z"/></svg>
@@ -78,7 +90,7 @@ export default {
     </div>
 </template>
 
-<style>
+<style scoped>
 
 span#openQRModal {
     margin-left: 1rem;
@@ -129,6 +141,19 @@ span#openQRModal:hover svg, span#openQRModal.active svg {
     padding: 1rem;
     box-sizing: border-box;
     text-align: center;
+    transition: border 100ms linear;
+}
+
+.topic-box-container textarea.invalid {
+    border: solid darkred;
+}
+
+p#invalid-input-text {
+    color: darkred;
+    font-family: sans-serif;
+    margin: 0.4rem 0 0 0;
+    padding: 0;
+    font-size: 0.8rem;
 }
 
 button#say-it-button {
@@ -145,11 +170,11 @@ button#say-it-button {
     transition: box-shadow 0.1s linear;
 }
 
-button:hover, button:focus-visible {
+button#say-it-button:hover, button#say-it-button:focus-visible {
     box-shadow: 0 0 0 0.2rem #AAA;
 }
 
-button:active {
+button#say-it-button:active {
     box-shadow: 0 0 0 0.3rem #999;
 }
 
